677cf773ffe3137f3f3f6a4e1e583e5668cd8400
ALI SHAIKH 1973 <139847421+alishaikh1973@users.noreply.github.com>
2023-09-17 03:54:24 +0530

    Add files via upload

---
+++ AtoZ - Copy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCodes 
+{
+    internal class AtoZ
+    {
+        static void Main (string[] args)
+        {
+            char i;
+            {
+                for (i = 'A'; i <= 'Z'; i++)
+                    Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
+
+    
+
+
+            
